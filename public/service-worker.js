d9e42cae26d5a62e573b6aebc7c7ab07307807a8
aeshna2008 <aeshna2008@gmail.com>
2025-10-26 22:16:45 -0700

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "silver-companion-v1";
+const urlsToCache = ["/", "/index.html", "/icon-512-512.png", "/vite.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
